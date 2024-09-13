048a6ed971bfde2e4a776887284b076cb2aa611b
Asman Haikal <52552889+e-haikal@users.noreply.github.com>
2024-09-13 20:32:01 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,59 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/** 
+* Create a loginFormElement variable for the form display.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+* Create inputEmailElement variable for email input display.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+* Create inputPasswordElement variable for password input display.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** 
+ * Create a variable expectedEmail to temporarily store email information.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/** 
+ * Create a variable expectedPassword to store temporary password information.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Comment : Adds click action to the button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+    /** 
+   * Create an email variable to store the email value obtained when the button is pressed.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+    
+  /** 
+   * Create a password variable to store the password value obtained when the button is pressed.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
+
+  /* Comment : Ensures that the email and password values ​​match the stored values. */
   if (email == expectedEmail && password == expectedPassword) {
+    /* Comment: If appropriate, the program will move to the home page. */
     goToHome();
   } else {
+    /* Comment : However, if it is not appropriate, it will display information that the input is incorrect. */
     showPopUp();
   }
 });
